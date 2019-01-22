99cf396232321f2a681930ea1ea10c5b71bdd385
huming <512657752@qq.com>
2019-01-22 11:12:19 +0800

    Tree Practice

--- src/main/java/org/hum/study/datastruct/tree/BinaryTreeUtils.java
+++ src/main/java/org/hum/study/datastruct/tree/BinaryTreeUtils.java
@@ -130,26 +130,89 @@ public class BinaryTreeUtils {
 					stack.push(node.right);
 				}
 			}
-			
 			return null;
 		}
 		
-		/**
-		 * 获得data到Root的节点路径
-		 * @param data
-		 * @return
-		 */
-		public static <T> Stack<T> findRootPath(T data) {
-			
+		// 获得data到Root的节点路径
+		public Stack<T> findRootPath(T data) {
+			// TODO
+			return null;
+		}
+
+		// 找到公共祖先
+		public TreeNode<T> findLCA(T data1, T data2) {
+			// TODO
 			return null;
 		}
 		
-		public static <T> TreeNode<T> findLCA(T data1, T data2) {
-			
+		// 统计数的高度
+		public int getTreeHeiht() {
+			// TODO
+			return 0;
+		}
+		
+		// 生成镜像树
+		public BinaryTree<T> createMirrorTree() {
+			// TODO
 			return null;
 		}
 		
-		public static BinaryTree<Integer> createSimple() {
+		// 判断是否是满二叉树
+		public boolean isFullBinaryTree() {
+			// TODO
+			return false;
+		}
+		
+		// 判断是否是完全二叉树
+		public boolean isCompleteBinaryTree() {
+			// TODO
+			return false;
+		}
+		
+		// 统计出叶子节点数量
+		public int getLeafCount() {
+			// TODO
+			return 0;
+		}
+		
+		// 统计出满叶子节点数量
+		public int getFullLeafCount() {
+			// TODO
+			return 0;
+		}
+		
+		// 判断两棵树是否相等
+		@Override
+		public boolean equals(Object obj) {
+			// TODO
+			return super.equals(obj);
+		}
+		
+		// 计算树的宽度
+		public int getWeight() {
+			// TODO
+			return 0;
+		}
+		
+		// 统计出同一层节点数据之和最大的层
+		public int getMaxLevelLayer() {
+			// TODO
+			return 0;
+		}
+		
+		// 打印出某节点的所有祖先节点
+		public Stack<TreeNode<T>> getAllAncestors(TreeNode<T> node) {
+			// TODO
+			return null;
+		}
+		
+		// Zigzag遍历
+		public void printZigzag() {
+			// TODO
+		}
+	
+		// 构建简单二叉树
+		public static BinaryTree<Integer> buildSimpleTree() {
 			BinaryTree<Integer> tree = new BinaryTree<Integer>();
 			TreeNode<Integer> node2 = new TreeNode<Integer>(2, new TreeNode<Integer>(4, null, null), new TreeNode<Integer>(5, null, null));
 //			TreeNode<Integer> node2 = new TreeNode<Integer>(2, new TreeNode<Integer>(4, null, null), null);
@@ -157,10 +220,26 @@ public class BinaryTreeUtils {
 			tree.root = new TreeNode<Integer>(1, node2, node3);
 			return tree;
 		}
+		
+		// 根据中序遍历和先序遍历构建二叉树
+		public static <T> BinaryTree<T> buildTree4MidAndPreOrder(T[] midArr, T[] preArr) {
+			// TODO
+			return null;
+		}
+		
+		// 给定任意遍历序列，尝试构建二叉树，构建失败抛出异常
+		public static <T> BinaryTree<T> buildTree4Common(T[] orderArr1, T[] ordeArr2) {
+			// TODO
+			// 注意：必须满足2个序列中有一个是中序遍历，因此尝试通过3种序列构建，如果都构建失败，则意味不满足构建Tree的序列
+			// 1.中序 + 先序
+			// 2.中序 + 后序
+			// 3.中序 + 层序
+			return null;
+		}
 	}
 	
 	public static void main(String[] args) {
 		// BinaryTree.createSimple().levelOrder();
-		System.out.println(BinaryTree.createSimple().findValue(31));
+		System.out.println(BinaryTree.buildSimpleTree().findValue(31));
 	}
 }
